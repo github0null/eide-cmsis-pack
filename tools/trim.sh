faf878b52984d8f51984c5b03c0c045ca35bb9ef
aly2_robot <robot@github0null.io>
2021-11-28 17:32:21 +0800

    support linux tools

--- tools/trim.sh
+++ tools/trim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FList = `find ../packages -name "*.pack"`
+FList=`find ../packages -name "*.pack"`
 
 for file in $FList
 do
     echo "=== Triming '$file'"
-    7za.exe d -tzip -r $file *.pe *.zip *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards/* BSP/* tools/* > nul
+    7za d -tzip -r $file *.pe *.zip *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards/* BSP/* tools/* > /dev/null
     echo ""
 done
