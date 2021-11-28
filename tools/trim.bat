03fb8714ddf8727f93971fdd965c19c2e3112a4d
null <me@github0null.io>
2021-11-28 16:48:52 +0800

    add pkgs

--- tools/trim.bat
+++ tools/trim.bat
@@ -2,7 +2,7 @@
 
 for %%i in (..\packages\*.pack) do ^
 echo === Triming '%%i' && ^
-7za.exe d -tzip -r %%i *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards\* > nul && ^
+7za.exe d -tzip -r %%i *.pe *.zip *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards\* BSP\* tools\* > nul && ^
 echo.
 
 pause
