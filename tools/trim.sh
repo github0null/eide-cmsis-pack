e8e99370bc0b22345b3c3ad84705714105a7bc2e
aly2_robot <robot@github0null.io>
2021-11-28 17:57:49 +0800

    update linux script

--- tools/trim.sh
+++ tools/trim.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-FList=`find ../packages -name "*.pack"`
+DEST=../packages/
+FList=`find ../temp -name "*.pack"`
 
 for file in $FList
 do
     echo "=== Triming '$file'"
-    7za d -tzip -r $file *.pe *.zip *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards/* BSP/* tools/* > /dev/null
+    ./7za d -tzip -r $file *.pe *.zip *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards/* BSP/* tools/* > /dev/null
+    mv -f $file $DEST
     echo ""
 done
