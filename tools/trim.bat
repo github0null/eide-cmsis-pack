d19709219223d46a614e6201bad2b816718a55c3
aly2_robot <robot@github0null.io>
2021-11-30 09:57:27 +0800

    add pkgs, fix script

--- tools/trim.bat
+++ tools/trim.bat
@@ -1,11 +1,11 @@
 @echo off
 
-set DEST = ..\packages\
+set DEST=..\packages\
 
 for %%i in (..\temp\*.pack) do ^
 echo === Triming '%%i' && ^
 7za.exe d -tzip -r %%i *.pe *.zip *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards\* BSP\* tools\* > nul && ^
-move /Y %%i %DEST% && ^
+echo move "%%i" to "%DEST%" && move /Y %%i %DEST% && ^
 echo.
 
 pause
--- tools/trim.sh
+++ tools/trim.sh
@@ -6,7 +6,6 @@ FList=`find ../temp -name "*.pack"`
 for file in $FList
 do
     echo "=== Triming '$file'"
-    ./7za d -tzip -r $file *.pe *.zip *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards/* BSP/* tools/* > /dev/null
-    mv -f $file $DEST
+    ./7za d -tzip -r $file *.pe *.zip *.FLM *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards/* BSP/* tools/* > /dev/null && mv -f $file $DEST
     echo ""
 done
