f607a6242544142b803eb4aa5da03c6f272af1f7
null <me@github0null.io>
2022-08-03 20:28:17 +0800

    update

--- tools/trim.bat
+++ tools/trim.bat
@@ -4,7 +4,7 @@ set DEST=..\packages\
 
 for %%i in (..\temp\*.pack) do ^
 echo === Triming '%%i' && ^
-7za.exe d -tzip -r %%i *.pe *.zip *.md *.chm *.pdf *.exe *.html *.jpg *.jpeg *.png *.js *.css Boards\* BSP\* tools\* > nul && ^
+7za.exe d -tzip -r %%i *.pe *.zip *.md *.chm *.pdf *.html *.jpg *.jpeg *.png *.js *.css Boards\* BSP\* tools\* > nul && ^
 echo move "%%i" to "%DEST%" && move /Y %%i %DEST% && ^
 echo.
 
